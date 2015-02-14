ec8462beef2bc8615abbf1174298705d4da13178
Rajshekar <rajshekarsv3@gmail.com>
2015-02-15 00:36:45 +0530

    wanna integrate

--- const.py
+++ const.py
@@ -1,1 +1,17 @@
 table_name = ["Unaudited Condensed Consolidated Interim Statements of Comprehensive Income","Unaudited Condensed Consolidated Interim Balance"]
+
+
+month_dict = {
+    'january' : ['jan','january'],
+    'febraury' : ['feb','febraury'],
+    'march' : ['mar','march'],
+    'april' : ['apr','april'],
+    'may' : ['may'],
+    'june' : ['jun','june'],
+    'july' : ['jul','july'],
+    'august' : ['aug','august'],
+    'september' : ['sept','September'],
+    'october' : ['oct','october'],
+    'november' : ['nov','november'],
+    'december' : ['dec','december']
+    }
--- parse.py
+++ parse.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup
 from lxml import etree
+from xlrd import open_workbook
 import re
 import const
 
 
 class ParserClass:
-
+	file_header = []
 	file_content = ''
+	work_book = ''
+	sheet = ''
 
 	def __init__(self,html_file):
 		i=0
@@ -34,16 +37,44 @@ class ParserClass:
 		# print(self.file_content.body.find_next_siblings('p'))
 		#print(self.file_content.contents)
 
-	def get_heading_column(self,table_row):
+	def get_heading_row(self,table_row):
 		for cell in table_row.findAll('td'):
-			print cell.getText()
+			if cell.getText():
+				self.file_header.append(cell.getText().replace('\n', '').replace('\r', '').replace('       ',' '))
+		
 
 
 	def get_all_column(self,table_content):
-		
 		for row in table_content.findAll('tr'):
 			for cell in row.findAll('td'):
-				print cell.getText()
+				print cell.getText().replace('\n', '').replace('\r', '').replace('       ',' ').replace('     ',' ')
+
+
+	def read_excel(self,excel_name,sheet_name):
+		self.work_book = open_workbook(excel_name)
+		self.sheet = self.work_book.sheet_by_name(sheet_name);
+		print 'Sheet Added:',self.sheet.name
+
+
+	def check_for_month_year_in_html(self,month,year):
+		for each_header in self.file_header:
+			for each_name in const.month_dict[month]:
+				if each_name in each_header:
+					if str(year) in each_header:
+						print each_header
+
+
+
+
+
+		# values = []
+		# for row in range(s.nrows):
+			
+		#  	#for col in range(s.ncols):
+		#  	if s.cell(row,0).value:
+	 # 			values.append(s.cell(row,0).value)
+		# print str(values)
+			 
 
 
 		
@@ -54,6 +85,9 @@ class ParserClass:
 
 html_analysis = ParserClass("data.htm")
 parsed_table = html_analysis.get_table("Unaudited Condensed Consolidated Interim Statements")
-html_analysis.get_heading_column(parsed_table.find('tr'))
-html_analysis.get_all_column(parsed_table)
+heading_row = html_analysis.get_heading_row(parsed_table.find('tr'))
+#html_analysis.get_all_column(parsed_table)
+html_analysis.read_excel('Model.xlsx','html')
+html_analysis.check_for_month_year_in_html('september',2013)
+print html_analysis.file_header
 #print parsed_table
